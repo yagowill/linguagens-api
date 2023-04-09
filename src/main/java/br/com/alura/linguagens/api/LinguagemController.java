9406193cc575cee7461d3267efbafbaa42d1550b
yagowill <yagowill3@gmail.com>
2023-04-09 20:40:57 -0300

    order by ranking and http status code 201

--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,12 +22,12 @@ public class LinguagemController {
 
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens() {
-        return repositorio.findAll();
+        return repositorio.findByOrderByRanking();
     }
 
     @PostMapping("/linguagens")
-    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
-        return repositorio.save(linguagem);
+    public ResponseEntity<Linguagem> cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        return new ResponseEntity<>(repositorio.save(linguagem), HttpStatus.CREATED);
     }
 
     @GetMapping("/linguagens/{id}")
